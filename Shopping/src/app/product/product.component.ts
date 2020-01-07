3e6e898a4f63145398cd4adc55efa8165cafdb27
riyaraval1512 <riya.raval@bacancytechnology.com>
2020-01-07 11:25:59 +0530

    Second

--- Shopping/src/app/product/product.component.ts
+++ Shopping/src/app/product/product.component.ts
@@ -8,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   ProductDetails =[
+    {id:"ID",compnayname:"Compnay Name", name:"Name"},
     {id:1, compnayname:"Suzuki", name:"OMNI"},
-    {id:2, compnayname:"Suzuki", name:"ECCO"}];   
+    {id:2, compnayname:"Suzuki", name:"ECCO"},
+    {id:3, compnayname:"Suzuki", name:"SX4"},
+    {id:4, compnayname:"Hyundai", name:"i10"},
+    {id:5, compnayname:"Hyundai", name:"i20"},
+    {id:6, compnayname:"Hyundai", name:"Tucson"}];   
 
 
 
